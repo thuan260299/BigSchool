1f9d7228df30dad549ca776560666fe6d9b9cef5
Administrator <Administrator@PC265>
2020-06-08 11:07:22 +0700

    lam toi page26

--- BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/Models/ApplicationDbContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace BigSchool.Models
 {
